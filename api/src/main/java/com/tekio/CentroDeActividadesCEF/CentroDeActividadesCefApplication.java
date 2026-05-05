189cb6e8257d4f494fe8a469d868e149d4226b76
matiAversa <matiasaversa1503@gmail.com>
2026-05-05 18:19:02 -0300

    agrego proyectos vacios de backend y frontend

---
+++ api/src/main/java/com/tekio/CentroDeActividadesCEF/CentroDeActividadesCefApplication.java
@@ -0,0 +1,13 @@
+package com.tekio.CentroDeActividadesCEF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroDeActividadesCefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroDeActividadesCefApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/tekio/CentroDeActividadesCEF/CentroDeActividadesCefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekio.CentroDeActividadesCEF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeActividadesCefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
