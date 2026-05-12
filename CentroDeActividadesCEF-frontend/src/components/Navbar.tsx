6c12c0f8539c950c45e2afb29d08fdc2d3afbb25
Facundo Saettone <facusae123@gmail.com>
2026-05-12 00:34:05 -0300

    Boton cerrar sesion

--- CentroDeActividadesCEF-frontend/src/components/Navbar.tsx
+++ CentroDeActividadesCEF-frontend/src/components/Navbar.tsx
@@ -1,25 +1,29 @@
 import "../styles/Navbar.css";
 
 function Navbar() {
-  return (
-    <header className="navbar">
-      <div>
-        <h1>Centro de Actividades</h1>
-        <span>Sistema de Gestión</span>
-      </div>
+	return (
+		<header className="navbar">
+			<div>
+				<h1>Centro de Actividades</h1>
+				<span>Sistema de Gestión</span>
+			</div>
 
-      <div className="user-box">
-        <div className="avatar">
-          A
-        </div>
+			<div className="user-box">
+				<div className="avatar">A</div>
 
-        <div>
-          <h4>Administrador</h4>
-          <p>Panel de control</p>
-        </div>
-      </div>
-    </header>
-  );
+				<div>
+					<h4>Administrador</h4>
+					<p>Panel de control</p>
+					<button
+						className="logout-btn"
+						onClick={() => (window.location.href = "/")}
+					>
+						Cerrar sesión
+					</button>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default Navbar;
